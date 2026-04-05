b983bb654e0ef0e50bcef237344d702eedad78c4
juniioroliveira <contato.nvt@hotmail.com>
2026-04-05 00:23:01 -0300

    chore: remove seed temporário

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,7 +8,4 @@ router.post('/login', authController.login);
 // Rota de Registro
 router.post('/register', authController.register);
 
-// [TEMP] Rota para restaurar o banco de dados oficial via arquivo de seed
-router.get('/force-seed', authController.forceSeed);
-
 module.exports = router;
