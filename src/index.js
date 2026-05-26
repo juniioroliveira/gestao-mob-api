3d4ca38008ba86f3eb10295557c3b5e6ac939c0a
juniioroliveira <contato.nvt@hotmail.com>
2026-05-26 01:10:55 -0300

    chore: auto-commit changes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' })); // Aumentado para suportar imagens base64
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// Rotas do Aplicativo
+// Rotas do Aplicativo 
 const testRoutes = require('./routes/testRoutes');
 const authRoutes = require('./routes/authRoutes');
 const homeRoutes = require('./routes/homeRoutes');
